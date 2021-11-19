973c74daa5cf106b0542fa835120a44321cdf0dc
fransbell <87742124+fransbell@users.noreply.github.com>
2021-11-19 18:11:20 +0700

    Delete build directory
    githu page are not supported so im moving to surge.sh

--- build/static/hjBVBrG4Uy4SFDlm0LK7L/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/8d9d41f0-c302706609ccd432.js","static/chunks/579-c6765e04eaec6fe0.js","static/chunks/pages/index-61089e42ebc6a4b9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
